61a1c9eb45b58fbb00da73316d3df1bff17de675
giacomopaolini1 <giacomopaolini08@gmail.com>
2023-06-28 16:52:03 +0200

    feat db

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,19 @@
 
 @section('content')
 <div class="container my-3">
-    <h1>Welcome Page</h1>
+    <h1 class="mb-3">Welcome Page</h1>
     <div class="row g-4">
         <div class="col">
-            <div>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, delectus ad esse illum omnis earum eligendi sint a minus quasi, inventore nulla autem. Maxime voluptatem eligendi veniam voluptates. Soluta, sunt!</p>
+            <div class="d-flex flex-wrap gap-3">
+                @foreach ($movies as $movie)
+                <div class="card p-3">
+                    <h1>{{ $movie->title}}</h1>
+                    <small>{{ $movie->original_title}}</small>
+                    <p>{{ $movie->nationality}}</p>
+                    <small>{{ $movie->date}}</small>
+                    <h3>{{ $movie->vote}}</h3>
+                </div>
+                @endforeach
             </div>
         </div>
     </div>
